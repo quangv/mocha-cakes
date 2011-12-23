59615595fe85bb0c4bf7098c536d1dc7962f7cd2
Quang Van <quangvvv@gmail.com>
2011-12-23 05:23:28 -0500

    Added compiled cakes.js

---
+++ cakes.js
@@ -0,0 +1,64 @@
+(function() {
+  var colors, depict, func, name, _ref;
+  var __slice = Array.prototype.slice;
+
+  colors = require('colors');
+
+  exports.Feature = function() {
+    var callback, feature, message, part, story, _i, _j, _len;
+    feature = arguments[0], story = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), callback = arguments[_i++];
+    message = ("Feature: " + feature + " \n\n").green.underline;
+    for (_j = 0, _len = story.length; _j < _len; _j++) {
+      part = story[_j];
+      message += '\t' + part + '\n';
+    }
+    describe(message, callback);
+  };
+
+  depict = function(label, args) {
+    if (args.length === 1) {
+      args[1] = args[0];
+      label = '';
+    }
+    return describe(label.replace('%s', args[0]), args[1]);
+  };
+
+  exports.Background = function(action, callback) {
+    return depict(action.magenta, arguments);
+  };
+
+  exports.Scenario = function() {
+    return depict("\n    Scenario: %s".green, arguments);
+  };
+
+  exports.Given = function() {
+    return depict("Given: %s", arguments);
+  };
+
+  exports.When = function() {
+    return depict(" When: %s", arguments);
+  };
+
+  exports.And = function() {
+    return depict("  and %s", arguments);
+  };
+
+  exports.Then = function() {
+    return depict(" Then: %s", arguments);
+  };
+
+  exports.But = function() {
+    return depict(" But %s", arguments);
+  };
+
+  exports.Spec = function() {
+    return depict('%s'.blue, arguments);
+  };
+
+  _ref = module.exports;
+  for (name in _ref) {
+    func = _ref[name];
+    global[name] = func;
+  }
+
+}).call(this);
